90dc6512af87a7e340c7d71cfe1b0bacd1f89573
安井 利紗 <yasui05821@gmail.com>
2017-01-06 19:07:44 +0900

    登壇者情報の追加

--- frontend/data/speaker_lists.js
+++ frontend/data/speaker_lists.js
@@ -1,6 +1,16 @@
 module.exports = [
     {
         id: 1,
+        title: "HTMLスナップショット2016改",
+        detail : "昨年9月に東京で開催されたHTML5 Conferenceのセッション「HTMLスナップショット2016」を圧縮して改変するものです。",
+        link: "https://twitter.com/momdo_",
+        photo: "unknown.jpg",
+        name: "momdo",
+        profile: "非IT系会社員なWHATWG/W3C仕様アマチュア翻訳者。ウェブアクセシビリティ基盤委員会（WAIC）作業部会4（翻訳）オブザーバー。",
+        field: "markup"
+    },
+    {
+        id: 2,
         title: "flowtype による型のある世界入門",
         detail : "フロントエンド開発の大規模化に伴い、動的型付け言語である JavaScript においても 静的な「型」というのが求められてくるようになりました。本セッションでは、 JavaScript に「型」を提供する flowtype というツールについてご紹介したいと思います。",
         link: "https://twitter.com/sairoutine",
@@ -10,7 +20,27 @@ module.exports = [
         field: "engineer"
     },
     {
-        id: 2,
+        id: 3,
+        title: "Enduring CSS",
+        detail : "どうCSSを書いていけばCSS設計が破綻しないのか。OOCSS、BEM、SMACSSをはじめとする、多くのCSS設計方法論が考え出されましたが、実際にコードを書いていくと、それらの方法論だけでは解決できない問題が多く発生してきませんか？その問題の一つとして、CSSが複雑になりすぎてしまうという点が挙げられるでしょう。[Enduring CSS](http://ecss.io/)（ECSS）は、Ben Frain氏が、自身の考える良いCSSの設計手法について書いた書籍です。Enduringとは、「不朽の」「恒久的な」という意味合いを持っており、その名の通り、永続的にサイトを運用していくにはどうCSSを設計すればよいかという点にフォーカスしています。長年CSSの設計について悩んできた私に、Enduring CSSは新しい視点を与えてくれました。このセッションでは、Enduring CSSの考え方の概要を紹介します。",
+        link: "https://twitter.com/Takazudo",
+        photo: "takazudo.jpg",
+        name: "高津戸壮 (@Takazudo)",
+        profile: "Web制作会社、フリーランスを経て、株式会社ピクセルグリッドに入社。数多くのWebサイト、WebアプリケーションのHTML、CSS、JavaScript実装に携わってきた。受託案件を中心にフロント周りの実装、設計、テクニカルディレクションを行う。",
+        field: "designer"
+    },
+    {
+        id: 4,
+        title: "スタンドアロンAMPのススメ",
+        detail : "メルカリアッテでのAMP化移行作業",
+        link: "https://t32k.me/",
+        photo: "ishimoto.jpg",
+        name: "石本 光司",
+        profile: "株式会社ソウゾウというところでメルカリアッテというクラシファイドサービスを作っています。メルカリじゃないです。",
+        field: "markup"
+    },
+    {
+        id: 5,
         title: "WebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
         detail : "WebGL 2.0 がいよいよやってきますね。Webの世界においても求められるグラフィックスの質やレベルは日々変わっていっています。今現在の WebGL の立ち位置と、これから先の WebGL の可能性,、そして WebGL 2.0 の登場で見えてくるそのさらに先の世界について考えてみましょう。",
         link: "https://twitter.com/h_doxas",
@@ -18,6 +48,16 @@ module.exports = [
         name: "杉本 雅広",
         profile: "サンデープログラマから最近やっと本業プログラマになりました。wgld.org、WebGL 総本山などを運営しています。",
         field: "engineer"
+    },
+    {
+        id: 6,
+        title: "エンジニアとデザイナーとの距離",
+        detail : "CSS設計に関する話題をよく聞くようになりました。 私自身、CSSの設計を実践して感じたことは、「エンジニアだけでCSSの設計をすることはできないのではないか」ということです。 私が何をして、何を感じて、どう改善していこうと考えているのかをお伝えします。技術的な話はありませんので、いろいろな職種の方に聞いていただきたいと思っています。",
+        link: "https://github.com/manabuyasuda",
+        photo: "unknown.jpg",
+        name: "安田学",
+        profile: "大阪にあるWeb制作会社TAMでマークアップエンジニアをしています。CSS設計が得意です。 不定期ですが、弊社コワーキングスペースで「CSSTalk」というCSSを主題とした勉強会を主催しています。",
+        field: "markup"
     },
     // {
     //     title: "懇親会プレゼンWebGL 2.0時代の幕開け - ウェブのグラフィックスはどう変わるのか",
