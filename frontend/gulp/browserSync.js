e28995d5a426d85210f1e4070143d684d30c7d51
t.goto <t.goto@chatbox-inc.com>
2017-01-27 13:55:13 +0900

    update

--- frontend/gulp/browserSync.js
+++ frontend/gulp/browserSync.js
@@ -10,20 +10,26 @@ const {src,dest} = global;
 
 gulp.task("server",()=> {
 
-    var config = {
-        proxy: "127.0.0.1:8000",
-        open: "external",
-        //notify: false
-    };
+    // var config = {
+    //     proxy: "127.0.0.1:8000",
+    //     open: "external",
+    //     //notify: false
+    // };
 
-    var server = {
-        base: `${dest}`,
-    }
+    // var server = {
+    //     base: `${dest}`,
+    // }
 
-    php.server(server,() => {
-        browserSync(config)
-    });
+    // php.server(server,() => {
+    //     browserSync(config)
+    // });
+    browserSync({
+        server:{
+            baseDir:"public",
+            index: "index.html"
 
+        }
+    })
     return gulp.watch(`${dest}/**/*`, () => {
         setTimeout(function(){
             browserSync.reload();
